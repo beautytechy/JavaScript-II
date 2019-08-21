877fb08dff7a4abed93c99ee0269dc825bfcbbde
Nicole Hollis <nicole@emmabeauty.co>
2019-08-20 20:57:35 -0700

    Completed Stretch, Challenge 2

--- assignments/closure.js
+++ assignments/closure.js
@@ -18,16 +18,21 @@ console.log(myfunction(5,5,anyvariable));
 
 // ==== Challenge 2: Implement a "counter maker" function ====
 const counterMaker = () => {
-  // IMPLEMENTATION OF counterMaker:
-  // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-  // 2- Declare a function `counter`. It should increment and return `count`.
-  //      NOTE: This `counter` function, being nested inside `counterMaker`,
-  //      "closes over" the `count` variable. It can "see" it in the parent scope!
-  // 3- Return the `counter` function.
+
+let count = 0;
+let counter = () => {
+  count = count + 1;
+  return count;
+}
+return counter;
 };
-// Example usage: const myCounter = counterMaker();
-// myCounter(); // 1
-// myCounter(); // 2
+
+const myCounter = counterMaker();
+
+console.log(myCounter());
+console.log(myCounter());
+console.log(myCounter());
+console.log(myCounter());
 
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
